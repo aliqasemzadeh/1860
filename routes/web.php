db48b3ab25c5e6ddbb66e2529d588c92695c9ad6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-12-01 11:37:15 +0330

    Add Persian language translations and integrate TailwindCSS for vendor log viewer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
+Route::get('/login', \App\Livewire\Auth\Login::class)->name('login');
