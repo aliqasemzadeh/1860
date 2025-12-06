90da1f80994a4c287e87513f1c0ca88818a7a4d2
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-12-06 10:53:16 +0330

    "Add repair services modal with flyout, headings, and responsive design in Service Center"

--- resources/views/livewire/service-center/repair/services.blade.php
+++ resources/views/livewire/service-center/repair/services.blade.php
@@ -1,3 +1,18 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-</div>
+<flux:modal name="service-center.repair.services.modal" class="md:w-2/3" flyout position="right">
+    @isset($this->repair)
+        <div class="flex flex-col gap-6">
+            <div class="flex items-center justify-between">
+                <div>
+                    <flux:heading size="lg" level="2">
+                        {{ __('app.repair_services') }}
+                    </flux:heading>
+                    <flux:subheading size="md">
+                        {{ __('app.repair_services_description') }}
+                    </flux:subheading>
+                </div>
+            </div>
+        </div>
+
+
+    @endisset
+</flux:modal>
