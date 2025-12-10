9e6526c88f1fffb456c28df422ee87ff9d1f349f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-12-10 17:09:31 +0330

    "Add Livewire CreateTransaction component and associated view for bank transaction creation"

---
+++ app/Livewire/Panel/Accounting/Bank/CreateTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel\Accounting\Bank;
+
+use Livewire\Component;
+
+class CreateTransaction extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.accounting.bank.create-transaction');
+    }
+}
