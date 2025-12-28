079dc0a9bf45297edaed7e610b1cf30a1306fe66
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-12-28 09:09:58 +0330

    "Add PriceFetcher model, migration, Livewire component, and Blade view for managing and displaying product price fetchers"

---
+++ app/Livewire/Shop/Product/PriceFetchers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Shop\Product;
+
+use Livewire\Component;
+
+class PriceFetchers extends Component
+{
+    public function render()
+    {
+        return view('livewire.shop.product.price-fetchers');
+    }
+}
