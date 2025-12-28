53377d6b584583aa61cf4c6d9e6f40f5ea897e50
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-12-28 09:17:34 +0330

    "Add UpdatePriceJob, FetchPriceJob, and PriceFetcherCommand for handling product price updates and fetching"

---
+++ app/Console/Commands/Shop/PriceFetcherCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Shop;
+
+use Illuminate\Console\Command;
+
+class PriceFetcherCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:price-fetcher-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
