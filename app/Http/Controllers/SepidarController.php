056b417b764802cd7c349e45cd74b7cc85d993f2
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-13 09:28:30 +0330

    save grouping

--- app/Http/Controllers/SepidarController.php
+++ app/Http/Controllers/SepidarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Jobs\Sepidar\SaveBankAccountDataJob;
 use App\Jobs\Sepidar\SaveInvoiceDataJob;
 use App\Jobs\Sepidar\SaveItemDataJob;
+use App\Jobs\Sepidar\SaveGroupingDataJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -34,6 +35,15 @@ class SepidarController extends Controller
 
         SaveItemDataJob::dispatch($request->all());
 
-        return response()->json(['message' => 'Bank account data received and queued for processing']);
+        return response()->json(['message' => 'Items data received and queued for processing']);
+    }
+
+    public function grouping(Request $request)
+    {
+        Log::channel('sepidar')->info('Grouping POST data received', $request->all());
+
+        SaveGroupingDataJob::dispatch($request->all());
+
+        return response()->json(['message' => 'Grouping data received and queued for processing']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/sepidar/invoices', [\App\Http\Controllers\SepidarController::class, 'invoices'])->name('sepidar.invoices');
 Route::post('/sepidar/bank-accounts', [\App\Http\Controllers\SepidarController::class, 'bank_accounts'])->name('sepidar.bank-accounts');
 Route::post('/sepidar/items', [\App\Http\Controllers\SepidarController::class, 'items'])->name('sepidar.items');
+Route::post('/sepidar/grouping', [\App\Http\Controllers\SepidarController::class, 'grouping'])->name('sepidar.grouping');
