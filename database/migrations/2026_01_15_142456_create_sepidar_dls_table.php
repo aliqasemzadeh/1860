2d38ec871aaa6c38789118ede1126861d386f77b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-15 16:50:00 +0330

    database

--- database/migrations/2026_01_15_142456_create_sepidar_dls_table.php
+++ database/migrations/2026_01_15_142456_create_sepidar_dls_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sepidar_dl_s', function (Blueprint $table) {
+        Schema::create('sepidar_dls', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('DLId')->unique();
             $table->string('Code')->nullable();
