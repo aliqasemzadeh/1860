24d4b49a8cf03216af9dfea578c746a4434ab784
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-18 10:11:46 +0330

    make relation banks

--- app/Models/Sepidar/RPA/BankAccount.php
+++ app/Models/Sepidar/RPA/BankAccount.php
@@ -3,8 +3,14 @@
 namespace App\Models\Sepidar\RPA;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BankAccount extends Model
 {
     public $table = 'sepidar_bank_accounts';
+
+    public function bankBranch(): BelongsTo
+    {
+        return $this->belongsTo(BankBranch::class, 'BankBranchRef', 'BankBranchId');
+    }
 }
--- app/Models/Sepidar/RPA/BankBranch.php
+++ app/Models/Sepidar/RPA/BankBranch.php
@@ -3,8 +3,14 @@
 namespace App\Models\Sepidar\RPA;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BankBranch extends Model
 {
     public $table = 'sepidar_bank_branches';
+
+    public function bank(): BelongsTo
+    {
+        return $this->belongsTo(BankBranch::class, 'BankRef', 'BankId');
+    }
 }
