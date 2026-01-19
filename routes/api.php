6eaeb9eadcaebef81251235d9a7f9c2d8b1904af
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-19 12:21:15 +0330

    Pyament  Cheque.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/sepidar', \App\Http\Controllers\SepidarController::class)->name('sepidar.invoices');
+Route::post('/sepidar/any', \App\Http\Controllers\SepidarController::class)->name('sepidar.invoices');
 
 Route::post('/sepidar/invoices', [\App\Http\Controllers\SepidarController::class, 'invoices'])->name('sepidar.invoices');
 Route::post('/sepidar/invoice-items', [\App\Http\Controllers\SepidarController::class, 'invoice_items'])->name('sepidar.invoice-items');
