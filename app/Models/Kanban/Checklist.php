6999922efef695c5225a3976c63069a76462d231
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-24 14:09:41 +0330

    View Invoice

---
+++ app/Models/Kanban/Checklist.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Kanban;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Checklist extends Model
+{
+    //
+}
---
+++ database/migrations/2026_01_24_134430_create_checklists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('card_id');
+
+            $table->string('title')->default('Checklist');
+            $table->unsignedInteger('position')->default(0);
+
+            $table->timestamps();
+
+            $table->index(['card_id', 'position']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checklists');
+    }
+};
