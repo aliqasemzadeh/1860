9abd4330568c706870ca2fde8e2d59b4dd8e39b3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-24 18:24:09 +0330

    Kanban Board.

---
+++ config/kanban.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Default Kanban Workspace
+     |--------------------------------------------------------------------------
+     |
+     | Workspace that will be used for all users by default.
+     | You can reference by ID or by a unique key/slug.
+     |
+     */
+    'default_workspace_id' => env('KANBAN_DEFAULT_WORKSPACE_ID', 1),
+];
