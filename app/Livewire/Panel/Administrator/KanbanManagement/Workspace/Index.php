6dfd6cfc71294aa001b7e0a78f9920bbfb4feca9
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-01-25 14:05:01 +0330

    Kanban Board.

--- app/Livewire/Panel/Administrator/KanbanManagement/Workspace/Index.php
+++ app/Livewire/Panel/Administrator/KanbanManagement/Workspace/Index.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class Index extends Component
 {
+
     public function render()
     {
         return view('livewire.panel.administrator.kanban-management.workspace.index');
