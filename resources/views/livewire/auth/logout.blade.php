a026fa4b7f34840ed4539f344d8dcb9337a11e3b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-13 10:07:32 +0330

    Update `logout.blade.php` to use `general` translation keys instead of `app`

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,24 +1,24 @@
 <x-slot name="title">
-    {{ __('app.logout.title') }}
+    {{ __('general.logout_title') }}
 </x-slot>
 <div class="max-w-md mx-auto px-4 py-8">
     <div class="flex items-start gap-4">
         <div class="flex-1">
-            <flux:heading size="lg">{{ __('app.logout.title') }}</flux:heading>
+            <flux:heading size="lg">{{ __('general.logout_title') }}</flux:heading>
             <flux:text class="mt-2 leading-relaxed">
-                {{ __('app.logout.message_line_1') }}<br>
-                {{ __('app.logout.message_line_2') }}
+                {{ __('general.logout_message_line_1') }}<br>
+                {{ __('general.logout_message_line_2') }}
             </flux:text>
         </div>
     </div>
 
     <div class="mt-8 flex items-center gap-4">
         <flux:button variant="primary" wire:click="cancel">
-            {{ __('app.logout.cancel') }}
+            {{ __('general.cancel') }}
         </flux:button>
 
         <flux:button variant="danger" wire:click="confirmLogout">
-            {{ __('app.logout.confirm') }}
+            {{ __('general.confirm') }}
         </flux:button>
     </div>
 </div>
