c47b3f710d1dd3abcac3f0301b4926f2dfebc581
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-13 10:06:18 +0330

    Update OTP SMS templates to include branding and formatting adjustments

--- lang/en/otp.php
+++ lang/en/otp.php
@@ -3,5 +3,5 @@
 return [
     // OTP SMS message text - customize as you like
     // Use :code placeholder to insert the OTP value
-    'sms_text' => 'Your login code: :code',
+    'sms_text' => "1860.ai\ncode: :code\n@1860.ai #:code",
 ];
