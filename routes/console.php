6c517f0228e1e37ba1593ff05264c90f463607c4
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-13 09:43:11 +0330

    remove outdated schedule entries for old remittance deletion and price fetcher updates

--- routes/console.php
+++ routes/console.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 
-// Schedule: remove old remittances every day at 00:00:00
-Schedule::command('app:remove-old-remittances-command')->dailyAt('00:00');
-
-// Schedule: update price fetchers every day at 22:00:00 (10 PM)
-Schedule::command('shop:update-price-fetchers')->dailyAt('22:00');
-
 // Schedule: cancel unpaid orders every minute
 Schedule::command('shop:cancel-unpaid-orders')->everyMinute();
 
