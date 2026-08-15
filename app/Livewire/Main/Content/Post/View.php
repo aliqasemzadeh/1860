ff5c0b049ae17d338a27ce19470c54a5e182e03d
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-15 11:10:30 +0330

    Add Livewire components for post index and view pages

---
+++ app/Livewire/Main/Content/Post/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Main\Content\Post;
+
+use Livewire\Component;
+
+class View extends Component
+{
+    public function render()
+    {
+        return view('livewire.main.content.post.view');
+    }
+}
