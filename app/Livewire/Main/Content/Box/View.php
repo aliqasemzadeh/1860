5e10ac697e11594085d156ef2b4cc7e279fd3620
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-15 12:28:11 +0330

    Add computed properties and pagination to Box View Livewire component for products and articles

--- app/Livewire/Main/Content/Box/View.php
+++ app/Livewire/Main/Content/Box/View.php
@@ -2,10 +2,42 @@
 
 namespace App\Livewire\Main\Content\Box;
 
+use App\Models\Content\Box;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class View extends Component
 {
+    use WithPagination;
+
+    public $id;
+    public $slug;
+
+    public function mount($id, $slug = null)
+    {
+        $this->id = $id;
+        $this->slug = $slug;
+    }
+
+    #[Computed]
+    public function box()
+    {
+        return Box::query()->findOrFail($this->id);
+    }
+
+    #[Computed]
+    public function products()
+    {
+        return $this->box->products()->select('products.*')->withEffectivePrice()->paginate(12, pageName: 'products-page');
+    }
+
+    #[Computed]
+    public function articles()
+    {
+        return $this->box->posts()->paginate(10, pageName: 'articles-page');
+    }
+
     public function render()
     {
         return view('livewire.main.content.box.view');
