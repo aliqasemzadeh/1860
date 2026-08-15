d12558669b98c700956476238da77480d7b7050f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-15 12:30:15 +0330

    Add pagination, search, and delete functionality to Box Index component; implement product listing and save logic in Box Create component

--- app/Livewire/Panel/Content/Box/Create.php
+++ app/Livewire/Panel/Content/Box/Create.php
@@ -2,10 +2,32 @@
 
 namespace App\Livewire\Panel\Content\Box;
 
+use App\Livewire\Forms\Content\BoxForm;
+use App\Models\Shop\Product;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Create extends Component
 {
+    use WithFileUploads;
+
+    public BoxForm $form;
+
+    #[Computed]
+    public function products()
+    {
+        return Product::query()->select('id', 'name')->get();
+    }
+
+    public function save()
+    {
+        $this->form->store();
+        $this->dispatch('panels.administrator.content.box.index.table');
+        \Flux::modals()->close();
+        \Flux::toast(__('general.created_successfully'));
+    }
+
     public function render()
     {
         return view('livewire.panel.content.box.create');
--- app/Livewire/Panel/Content/Box/Index.php
+++ app/Livewire/Panel/Content/Box/Index.php
@@ -2,10 +2,35 @@
 
 namespace App\Livewire\Panel\Content\Box;
 
+use App\Models\Content\Box;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
+    #[Computed]
+    #[On('panels.administrator.content.box.index.table')]
+    public function boxes()
+    {
+        return Box::query()
+            ->when($this->search, fn($q) => $q->where('title_fa', 'like', "%{$this->search}%")->orWhere('title_en', 'like', "%{$this->search}%"))
+            ->ordered()
+            ->paginate(config('general.per_page'));
+    }
+
+    public function delete(int $id)
+    {
+        Box::query()->findOrFail($id)->delete();
+        \Flux::toast(__('general.deleted_successfully'));
+        $this->dispatch('panels.administrator.content.box.index.table');
+    }
+
     public function render()
     {
         return view('livewire.panel.content.box.index');
