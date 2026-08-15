1ff615a686533e7f75abb18c36284a3654fd4975
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-15 11:39:08 +0330

    Add Livewire components for managing boxes: Create, Edit, and Index views

---
+++ app/Livewire/Panel/Content/Box/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel\Content\Box;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.content.box.create');
+    }
+}
---
+++ app/Livewire/Panel/Content/Box/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel\Content\Box;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.content.box.edit');
+    }
+}
