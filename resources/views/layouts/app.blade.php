eeb8307ec5af794d08a6d1500f238c9fb4d63941
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-20 10:25:04 +0330

    Add eNAMAD trust seal logo to the app layout for improved trust and credibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
 
     <flux:sidebar.spacer />
 
+    <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=709965&Code=xXpQ3qR4I1hEHCK1nsAaXGNdIJhuSmol'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=709965&Code=xXpQ3qR4I1hEHCK1nsAaXGNdIJhuSmol' alt='' style='cursor:pointer' code='xXpQ3qR4I1hEHCK1nsAaXGNdIJhuSmol'></a>
+
     <flux:sidebar.nav>
             <flux:sidebar.item icon="newspaper" href="{{ route('post.index') }}" wire:navigate :current="request()->routeIs('post.*')">{{ __('general.blog') }}</flux:sidebar.item>
             <flux:sidebar.item icon="mail" href="{{ route('contact.index') }}">{{ __('general.contact_us') }}</flux:sidebar.item>
